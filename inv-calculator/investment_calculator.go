ad1b1a3ee9c5a9de6bbe313a9d86ceac29d251a2
Francois Somville <francoissomville@hotmail.com>
2025-12-08 15:07:01 +0100

    Initial commit

---
+++ inv-calculator/investment_calculator.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	const inflationRate = 2.5
+
+	var investedAmount, years, expectedReturnRate float64
+
+	fmt.Print("How much do you want to invest? ")
+	fmt.Scan(&investedAmount)
+
+	fmt.Print("How many years? ")
+	fmt.Scan(&years)
+
+	fmt.Print("What is the expected return rate (in %)? ")
+	fmt.Scan(&expectedReturnRate)
+
+	futureValue := investedAmount * math.Pow(1+expectedReturnRate/100, years)
+
+	futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
+
+	fmt.Print("Future Value: ")
+	fmt.Println(math.Round(futureValue))
+	fmt.Print("Future Real Value: ")
+	fmt.Println(math.Round(futureRealValue))
+}
