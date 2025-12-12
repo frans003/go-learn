a90cbb1801bcd3ecdeee4d75c8a324a75c4a94f3
Francois Somville <francoissomville@hotmail.com>
2025-12-12 11:39:37 +0100

    More practice

--- profit-calculator/profit-calculator.go
+++ profit-calculator/profit-calculator.go
@@ -4,11 +4,15 @@ import "fmt"
 
 func main() {
 
-	var revenues, expenses, taxRate float64 = calcProfit()
+	var revenues, expenses, taxRate float64
+	revenues = getUserInput("What are the company revenues? ")
+	expenses = getUserInput("What are the total expenses? ")
+	taxRate = getUserInput("What is the tax rate? ")
 
 	EBT := calcEarningsBeforeTaxes(revenues, expenses)
 	profits, taxes := calcProfits(EBT, taxRate)
 	ratio := calcRatio(EBT, profits)
+
 	fmt.Printf(`Your revenues before taxes are: €%.2f
 Total taxes: €%.2f
 Earnings after tax: €%.2f
@@ -16,19 +20,6 @@ Ratio: %.1f`, EBT, taxes, profits, ratio)
 
 	formattedProfit := fmt.Sprintf("\nRevenues: €%.2f", profits)
 	fmt.Print(formattedProfit)
-}
-
-func calcProfit() (revenues, expenses, taxRate float64) {
-	fmt.Print("What are the company revenues? ")
-	fmt.Scan(&revenues)
-
-	fmt.Print("What are the total expenses? ")
-	fmt.Scan(&expenses)
-
-	fmt.Print("What is the tax rate? ")
-	fmt.Scan(&taxRate)
-
-	return revenues, expenses, taxRate
 }
 
 func calcEarningsBeforeTaxes(revenues, expenses float64) (ebt float64) {
@@ -45,4 +36,11 @@ func calcProfits(EBT, taxRate float64) (profits, taxes float64) {
 func calcRatio(EBT, profits float64) (ratio float64) {
 	ratio = EBT / profits
 	return ratio
+}
+
+func getUserInput(prompt string) float64 {
+	var userInput float64
+	fmt.Println(prompt)
+	fmt.Scan(&userInput)
+	return userInput
 }
